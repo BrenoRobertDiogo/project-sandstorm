e05848f21715faeab7ed8841d64576d870ed821a
AquilesAguiar <aquilessilva@ucl.br>
2020-07-22 13:57:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,37 @@ msgError = '<h1> Método ou site não encontrado <h1> <script>window.alert()</sc
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method=='GET':
-        return render_template('link.html')
+        return render_template('login.html')
     else:
         return msgError
 
 """@app.route('/dados', methods=['POST', 'GET'])
 def dados():"""
 
-@app.route("/transacoes",methods=["POST"])
+
+@app.route("/link",methods=["POST"])
+
+def link():
+    if request.method == "POST":
+        login = request.form.to_dict()
+        
+        nome = login["id"]
+        senha = login["pass"]
+        
+        client = Client(f"{nome}", 
+        f"{senha}", 
+        "https://sandbox.belvo.co")
+
+        instituicoes = []
+
+        [instituicoes.append(x) for x in client.Institutions.list()]
+
+        link = []
+
+        [link.append(x) for x in client.Links.list()]
+        
+        return render_template("link.html",id=nome,senha=senha,links = link,instituicoes=instituicoes,
+    numero_instituicoes = len(instituicoes),numero_links=len(link))
 
 def transacoes():
     login = request.form.to_dict()
