a653c668beeb41eaa8835e7f6d9f9f7e7704ab8d
BrenoRobertDiogo <brenorobert123@gmail.com>
2020-07-25 10:53:12 -0300

    Alterações no login

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ def transformers(idPessoa, senhaPessoa):
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    resp = make_response(render_template('login.html'))
     if request.method=='GET':
-        return render_template('login.html')
+        return resp#render_template('login.html')
     else:
         return msgError
 
@@ -33,22 +34,31 @@ def dados():
         nome = request.form['idPessoa']           #id
         senha = request.form['senha']             #senha
         client = bibliotecas.chamar(nome, senha)  #Cazendo o client
-        x = [x for x in str(client)]
-        return x
+        #x = [x for x in str(client)]
+        #return str(x)
         contas = bibliotecas.retornaContas(client)#Contas que tem la
         tamanho = len(contas)                     #Quantas contas tem
         
+        #return str(contas)
         ###Fazendo os cookies
+        valor = str(contas[0])
+        #return valor
         resp = make_response(render_template('index.html', nome=contas, tamanho=tamanho))#Site a ser retornado
         resp.set_cookie('idPessoa', json.dumps(nome))#id
         resp.set_cookie('senhaPessoa', json.dumps(senha))#senha
-        resp.set_cookie('clientPessoa', json.dumps(client))
+        resp.set_cookie('teste', json.dumps(valor))#json.dumps(contas[0]))
+        for x in range(tamanho):
+            resp.set_cookie(f'id{x}', json.dumps(str(contas[x])))
+        cookie1 = request.cookies.get('idPessoa')
+        cookie3 = request.cookies.get('teste')
+        #resp.set_cookie('clientPessoa', json.dumps(client))
         return resp
 @app.route('/rota', methods=['POST', 'GET'])
 def rota():
     cookie1 = request.cookies.get('idPessoa')
     cookie2 = request.cookies.get('senhaPessoa')
-    return f"{cookie1}, {cookie2}"
+    cookie3 = request.cookies.get('teste')
+    return f"{cookie1}, {cookie2}, {cookie3}"
 
 
 @app.route("/link",methods=["POST", "GET"])
@@ -87,6 +97,3 @@ def contato():
 
 if __name__=='__main__':
     app.run(debug=True)
-
-
-
